f5e6f1f53fd6e1e36e62ae04c8a0ffb80bbada11
Eve0784 <everceymedina@gmail.com>
2025-10-21 10:23:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+// console.log('Hello World!');
+
+//boolean
+true;
+false;
+
+//number
+3;
+3.14;
+-34.4;
+Infinity;  // numero piu grande
+-Infinity;
+NaN;  // not a number
+
+//string
+'ciao mondo';
+"ciao mondo";
+"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, porro dicta dolor repellat amet culpa autem, perspiciatis quaerat debitis velit labore ducimus odit fuga quibusdam! Autem ipsam earum tempore similique.";
+'a';
+'';
+
+
+//tipi nulli
+undefined; // non definito non è stata mai riempita
+null; //nullo
+
+//-------------------OPERATORI LOGICI-------------------------------
+//NOT (!)
+true; //vero
+!true; // falso
+
+false; //falso
+!false; // vero
+// console.log(!false);
+
+//AND (&&)
+true && true; //true
+true && false; // false
+false && true; // false
+false && false; //false
+// console.log(true&& true);
+// console.log(true&&false);
+
+
+//OR (||)
+true ||true; //true
+true || false; // true
+false|| true; // true
+false|| false; //false
+
+// console.log(true||true);
+// console.log(true||false);
+// console.log(false||false);
+
+//OPERATORI MATEMATICI
+// somma (+)
+5+10; //15
+
+// sottrazione (-)
+-3.1;
+90-10; //80
+
+// moltiplicazione (*) 
+6*8; //48
+
+// divisione(/) 
+9/3; //3
+
+// potenza(**)
+2**3; //8
+4**2; //16
+
+// modulo(%) resto della divisione intera
+10 % 3; //1
+5 % 3; //2
+12 % 7; //5
+24 % 11; //2
+
+// somma (concatenazione) di stringhe (+)
+"ciao" + "mamma"; // "ciaomamma"
+"ciao " + "mamma"; // "ciao mamma"
+"ciao" + "mamma"; // "ciao mamma"
+
+//  OPERATORI DI CONFRONTO
+// uguaglianza (== o ===)
+5 === 5; //true
+7 === 2; // false
